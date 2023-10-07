e8742519831a2b36390300459b304d4e531b95b5
mosigaaa <mosiga2008@gmail.com>
2023-10-07 22:18:25 +0700

    "Feat: complete assignment by moss"

--- ex-2.js
+++ ex-2.js
@@ -2,14 +2,35 @@
 
 function atLeastFive(array, operation) {
   // Start coding here
+  return operation(array);
+}
+
+const checkresult = (array) => {
+  let countpass = 0;
+  let i = 0;
+  while (array[i]) {
+    if (array[i] > 70) {
+      countpass += 1;
+    }
+    i++;
+  }
+  if (countpass >= 5) {
+    return true;
+  } else {
+    return false;
+  }
 }
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
 const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
-// Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
-let scoreRoom3Result;
+// Using `atLeastFive` function here
+let scoreRoom1Result = atLeastFive(studentScoresRoom1, checkresult);
+let scoreRoom2Result = atLeastFive(studentScoresRoom2, checkresult);
+let scoreRoom3Result = atLeastFive(studentScoresRoom3, checkresult);
+
+console.log(scoreRoom1Result); 
+console.log(scoreRoom2Result); 
+console.log(scoreRoom3Result); 
